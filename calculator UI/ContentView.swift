4f6e0780970b2f9bad2af7c287affe981f89edcd
mathissjsp <mathis.goffin@jopie.be>
2025-02-20 11:04:29 +0100

    Knoppen toegevoegd

--- calculator UI/ContentView.swift
+++ calculator UI/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                Text("0").font(.largeTitle)
+            }
+            HStack{
+                Text("AC").padding().background(.gray).cornerRadius(9999).frame(width: 80, height: 80)
+                Image(systemName: "plus.forwardslash.minus").padding().background(.gray).cornerRadius(9999).frame(width: 80, height: 80)
+                Image(systemName: "percent").padding().background(.gray).cornerRadius(9999).frame(width: 80, height: 80)
+                Image(systemName: "divide").padding().background(.gray).cornerRadius(9999).frame(width: 80, height: 80)
+            }
+            
         }
-        .padding()
     }
 }
 
