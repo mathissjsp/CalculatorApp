08b4b1a7bb13a848cbc8d20eae50593ca436c0a6
mathissjsp <mathis.goffin@jopie.be>
2025-02-20 11:46:08 +0100

    klaar

--- calculator UI/ContentView.swift
+++ calculator UI/ContentView.swift
@@ -9,17 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            HStack{
-                Text("0").font(.largeTitle)
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                VStack(spacing: 0) {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {}) {
+                            Text("0").font(.system(size: 80)).foregroundColor(.white).padding(.horizontal, 40)
+                        }
+                    }
+                }
+
+                VStack {
+                    HStack(spacing: 10) {
+                        Button(action: {}) {
+                            Text("AC").foregroundColor(.white).font(.system(size: 30)).frame(width: 80, height: 80).background(.gray).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Image(systemName: "plus.forwardslash.minus").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(.gray).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Image(systemName: "percent").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(.gray).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Image(systemName: "divide").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(.orange).clipShape(Circle())
+                        }
+                    }
+                    HStack(spacing: 10) {
+                        Button(action: {}) {
+                            Text("7").foregroundColor(.white).font(.system(size: 30)).frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Text("8").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Text("9").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Image(systemName: "multiply").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(.orange).clipShape(Circle())
+                        }
+                    }
+                    HStack(spacing: 10) {
+                        Button(action: {}) {
+                            Text("4").foregroundColor(.white).font(.system(size: 30)).frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Text("5").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Text("6").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Image(systemName: "minus").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(.orange).clipShape(Circle())
+                        }
+                    }
+                    HStack(spacing: 10) {
+                        Button(action: {}) {
+                            Text("1").foregroundColor(.white).font(.system(size: 30)).frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Text("2").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Text("3").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Image(systemName: "plus").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(.orange).clipShape(Circle())
+                        }
+                    }
+                    HStack(spacing: 10) {
+                        Button(action: {}) {
+                            Image(systemName: "iphone").foregroundColor(.white).font(.system(size: 30)).frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Text("0").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Text(",").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(Color.gray.opacity(0.4)).clipShape(Circle())
+                        }
+                        Button(action: {}) {
+                            Image(systemName: "equal").foregroundColor(.white).font(.system(size: 30)).scaledToFill().frame(width: 80, height: 80).background(.orange).clipShape(Circle())
+                        }
+                    }
+                }
+                .padding()
             }
-            HStack{
-                Text("AC").padding().background(.gray).cornerRadius(9999).frame(width: 80, height: 80)
-                Image(systemName: "plus.forwardslash.minus").padding().background(.gray).cornerRadius(9999).frame(width: 80, height: 80)
-                Image(systemName: "percent").padding().background(.gray).cornerRadius(9999).frame(width: 80, height: 80)
-                Image(systemName: "divide").padding().background(.gray).cornerRadius(9999).frame(width: 80, height: 80)
-            }
-            
         }
     }
 }
