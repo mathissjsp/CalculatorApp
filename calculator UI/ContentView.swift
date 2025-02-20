a3c2f85448e88d47383045c18af00760225f74d4
mathissjsp <mathis.goffin@jopie.be>
2025-02-20 10:48:56 +0100

    Initial Commit

---
+++ calculator UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calculator UI
+//
+//  Created by mathis goffin on 20/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calculator UI/calculator_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_UIApp.swift
+//  calculator UI
+//
+//  Created by mathis goffin on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct calculator_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
